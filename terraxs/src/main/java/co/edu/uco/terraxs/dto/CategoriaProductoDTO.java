42b1edc34cdbfc85f8a3c45166a3ea87289c61fb
juan jose narvaez <juanjosenarvaezmarin13092005@gmail.com>
2025-05-07 22:03:51 -0500

    comit el cual agrega los entity y dtos de categoriaProducto

---
+++ terraxs/src/main/java/co/edu/uco/terraxs/dto/CategoriaProductoDTO.java
@@ -0,0 +1,69 @@
+package co.edu.uco.terraxs.dto;
+
+import java.util.UUID;
+
+import co.edu.uco.terraxs.crosscutting.utilitarios.UtilTexto;
+import co.edu.uco.terraxs.crosscutting.utilitarios.UtilUUID;
+import co.edu.uco.terraxs.entity.CategoriaProductoDTO;
+import co.edu.uco.terraxs.entity.CategoriaProductoEntity.Builder;
+import co.edu.uco.terraxs.crosscutting.utilitarios.UtilObjeto;
+
+public final class CategoriaProductoDTO{
+
+	private UUID id;
+	private String nombre;
+	private String descripcion;
+
+	public CategoriaProductoDTO(){
+		setId(UtilUUID.obtenerValorDefecto());
+		setNombre(UtilTexto.getInstance().obtenerValorDefecto());
+		setDescripcion(UtilTexto.getInstance().obtenerValorDefecto());
+
+	}
+
+	public CategoriaProductoDTO(final UUID id) {
+		setId(id);
+		setNombre(UtilTexto.getInstance().obtenerValorDefecto());
+		setDescripcion(UtilTexto.getInstance().obtenerValorDefecto());
+	}
+
+	public CategoriaProductoDTO(final UUID id, final String nombre,final String descripcion) {
+		setId(id);
+		setNombre(nombre);
+		setDescripcion(descripcion);
+	}
+
+	public static CategoriaProductoDTO obtenerValorDefecto() {
+		return new CategoriaProductoDTO();
+	}
+
+	public static CategoriaProductoDTO obtenerValorDefecto(final CategoriaProductoDTO categoriaProducto) {
+		return UtilObjeto.getInstance().obtenerValorDefecto(categoriaProducto, obtenerValorDefecto());
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(final UUID id) {
+		this.id = UtilUUID.obtenerValorDefecto(id);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(final String nombre) {
+		this.nombre = UtilTexto.getInstance().quitarEspaciosBlancoInicioFin(nombre);
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(final String descripcion) {
+		this.descripcion = UtilTexto.getInstance().quitarEspaciosBlancoInicioFin(descripcion);
+	}
+	
+
+}
---
+++ terraxs/src/main/java/co/edu/uco/terraxs/entity/CategoriaProductoEntity.java
@@ -0,0 +1,70 @@
+package co.edu.uco.terraxs.entity;
+
+import java.util.UUID;
+
+import co.edu.uco.terraxs.crosscutting.utilitarios.UtilTexto;
+import co.edu.uco.terraxs.crosscutting.utilitarios.UtilUUID;
+import co.edu.uco.terraxs.dto.TipoDocumentoDTO;
+import co.edu.uco.terraxs.dto.TipoDocumentoDTO.Builder;
+import co.edu.uco.terraxs.crosscutting.utilitarios.UtilObjeto;
+
+public final class CategoriaProductoEntity{
+
+	private UUID id;
+	private String nombre;
+	private String descripcion;
+
+	public CategoriaProductoEntity(){
+		setId(UtilUUID.obtenerValorDefecto());
+		setNombre(UtilTexto.getInstance().obtenerValorDefecto());
+		setDescripcion(UtilTexto.getInstance().obtenerValorDefecto());
+
+	}
+
+	public CategoriaProductoEntity(final UUID id) {
+		setId(id);
+		setNombre(UtilTexto.getInstance().obtenerValorDefecto());
+		setDescripcion(UtilTexto.getInstance().obtenerValorDefecto());
+	}
+
+	public CategoriaProductoEntity(final UUID id, final String nombre,final String descripcion) {
+		setId(id);
+		setNombre(nombre);
+		setDescripcion(descripcion);
+	}
+
+	public static CategoriaProductoEntity obtenerValorDefecto() {
+		return new CategoriaProductoEntity();
+	}
+
+	public static CategoriaProductoEntity obtenerValorDefecto(final CategoriaProductoEntity categoriaProducto) {
+		return UtilObjeto.getInstance().obtenerValorDefecto(categoriaProducto, obtenerValorDefecto());
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(final UUID id) {
+		this.id = UtilUUID.obtenerValorDefecto(id);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(final String nombre) {
+		this.nombre = UtilTexto.getInstance().quitarEspaciosBlancoInicioFin(nombre);
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(final String descripcion) {
+		this.descripcion = UtilTexto.getInstance().quitarEspaciosBlancoInicioFin(descripcion);
+	}
+	
+	
+
+}
