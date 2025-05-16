3880e1c7c985f0695eec3742979c43d59ad764f3
Juan Camilo Bernal Carmona <juancamilobernal2222@gmail.com>
2025-05-16 13:27:08 -0500

    Conexión con la Base de Datos a través de PostgreSQLDAOFactory

--- terraxs/src/main/java/co/edu/uco/terraxs/data/dao/factory/postgresql/PostgreSQLDAOFactory.java
+++ terraxs/src/main/java/co/edu/uco/terraxs/data/dao/factory/postgresql/PostgreSQLDAOFactory.java
@@ -1,74 +1,97 @@
 package co.edu.uco.terraxs.data.dao.factory.postgresql;
 
-
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import co.edu.uco.terraxs.data.dao.factory.DAOFactory;
 import co.edu.uco.terraxs.data.dao.entity.ciudad.CiudadDAO;
 import co.edu.uco.terraxs.data.dao.entity.ciudad.impl.postgresql.CiudadPostgreSQLDAO;
 import co.edu.uco.terraxs.data.dao.entity.departamento.DepartamentoDAO;
 import co.edu.uco.terraxs.data.dao.entity.departamento.impl.postgresql.DepartamentoPostgreSQLDAO;
 import co.edu.uco.terraxs.data.dao.entity.pais.PaisDAO;
 import co.edu.uco.terraxs.data.dao.entity.pais.impl.postgresql.PaisPostgreSQLDAO;
-
-
+import co.edu.uco.terraxs.data.dao.factory.DAOFactory;
 
 public class PostgreSQLDAOFactory extends DAOFactory {
-	
-	private Connection conexion;
-	
-	public PostgreSQLDAOFactory() {
-		abrirConexion();
-	}
-	
-	
-	
 
+    private static final String URL = "jdbc:postgresql://localhost:5432/DOO2025TERRAXS";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1036778928";
+    private static final String DRIVER = "org.postgresql.Driver";
 
-	@Override
-	protected void abrirConexion() {
-		
-		// TODO Auto-generated method stub
-		
-	}
+    private Connection conexion;
 
-	@Override
-	public void iniciarTransaccion() {
-		// TODO Auto-generated method stub
-		
-	}
+    public PostgreSQLDAOFactory() {
+        abrirConexion();
+    }
 
-	@Override
-	public void confirmarTransaccion() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    protected void abrirConexion() {
+        try {
+            Class.forName(DRIVER);
+            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+            conexion.setAutoCommit(false);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException("Error al abrir conexión con la base de datos PostgreSQL", e);
+        }
+    }
 
-	@Override
-	public void cancelarTransaccion() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void iniciarTransaccion() {
+        try {
+            if (conexion.getAutoCommit()) {
+                conexion.setAutoCommit(false);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al iniciar la transacción", e);
+        }
+    }
 
-	@Override
-	public void cerrarConexion() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void confirmarTransaccion() {
+        try {
+            conexion.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al confirmar la transacción", e);
+        }
+    }
 
-	@Override
-	public PaisDAO getPaisDAO() {
-		return new PaisPostgreSQLDAO(conexion);
-	}
+    @Override
+    public void cancelarTransaccion() {
+        try {
+            conexion.rollback();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al cancelar la transacción", e);
+        }
+    }
 
-	@Override
-	public DepartamentoDAO getDepartamentoDAO() {
-		return new DepartamentoPostgreSQLDAO(conexion);
-	}
+    @Override
+    public void cerrarConexion() {
+        try {
+            if (conexion != null && !conexion.isClosed()) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al cerrar la conexión", e);
+        }
+    }
 
-	@Override
-	public CiudadDAO getCiudadDAO() {
-		return new CiudadPostgreSQLDAO(conexion);
-	}
+    @Override
+    public PaisDAO getPaisDAO() {
+        return new PaisPostgreSQLDAO(conexion);
+    }
+
+    @Override
+    public DepartamentoDAO getDepartamentoDAO() {
+        return new DepartamentoPostgreSQLDAO(conexion);
+    }
+
+    @Override
+    public CiudadDAO getCiudadDAO() {
+        return new CiudadPostgreSQLDAO(conexion);
+    }
+    
 
 }
+
+
